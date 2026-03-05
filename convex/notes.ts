4c6ef1f41f3969e12fb9e540ed5ef28d1e924aa8
Drew <chukwukaenudeme@gmail.com>
2026-03-05 10:44:30 +0100

    added a cron job

--- convex/notes.ts
+++ convex/notes.ts
@@ -1,12 +1,12 @@
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { internalMutation, mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
-import { RateLimiter, MINUTE, HOUR } from "@convex-dev/rate-limiter";
+import { RateLimiter, MINUTE } from "@convex-dev/rate-limiter";
 import { components } from "./_generated/api";
 
 const rateLimiter = new RateLimiter(components.rateLimiter, {
-  createNotes: { kind: "fixed window", rate: 1, period: MINUTE },
+  createNotes: { kind: "fixed window", rate: 100, period: MINUTE },
 });
 
 export const createNotes = mutation({
@@ -33,5 +33,13 @@ export const getNotes = query({
       throw new Error("Not authenticated");
     }
     return await ctx.db.query("notes").collect();
+  },
+});
+
+export const deleteAll = internalMutation({
+  args: {},
+  handler: async (ctx) => {
+    const notes = await ctx.db.query("notes").collect();
+    await Promise.all(notes.map((note) => ctx.db.delete(note._id)));
   },
 });
